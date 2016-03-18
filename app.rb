9ae1a9ff51cbdd51d3166935d460d93465e0b1f6
Next <nextacademy@email.com>
2016-03-17 22:16:36 -0700

    undid changes to config file. Redirected the path to config/application.rb

--- app.rb
+++ app.rb
@@ -1,7 +1,48 @@
-# Pre-configurations (Change if you want an awesome difficulty assessment)
 require_relative 'config/application'
 
+def self.run(argv)
+	prompt = argv[0].to_s
+	add(argv) 		if prompt == "--add"
+	list 			if prompt == "--list"
+	update(argv) 	if prompt == "--update"
+	delete(argv) 	if prompt == "--delete"
+end
 
-# Your Code begins from this line onwards #
-app = Model.new
-app.to_s
+def add(details)
+	attributes = {}
+	attributes[:Name] 		= details[1].to_s
+	attributes[:Phone] 		= details[2].to_s
+	attributes[:Address] 	= details[3].to_s
+	Contact.create(attributes)
+end
+
+def list
+	list = Contact.all
+	puts "list".ljust(6) + "Name".ljust(30) + "Phone".ljust(15) + "Address"
+	puts "====".ljust(6) + "====".ljust(30) + "=====".ljust(15) + "======="
+	list.each do |x|
+		puts "#{x[:id]}".ljust(6) + "#{x[:Name]}".ljust(30) + "#{x[:Phone]}".ljust(15) + "#{x[:Address]}"
+	end
+end
+
+def update(details)
+	unless details[1].to_i > Contact.count
+		contact = Contact.find(details[1].to_i)
+		contact.update(Name: details[2].to_s, Phone: details[3].to_s, Address: details[4].to_s) unless details[2] == nil
+	else
+		puts "A contact of that ID does not exist."
+	end
+end
+
+def delete(id)
+	id = id[1].to_i
+	Contact.all.each {|x| @check = true if id == x[:id]}
+	if @check == true
+		contact = Contact.find(id)
+		contact.destroy
+	else
+		puts "A contact of that ID does not exist."
+	end
+end
+
+run(ARGV)
--- db/migrate/20160318_create_contacts.rb
+++ db/migrate/20160318_create_contacts.rb
@@ -1,9 +1,8 @@
-require_relative '../config'
+require_relative '../../config/application'
 
 class CreateContacts < ActiveRecord::Migration
 	def change
 		create_table :contacts do |t|
-			t.string :list
 			t.string :Name
 			t.string :Phone
 			t.string :Address
